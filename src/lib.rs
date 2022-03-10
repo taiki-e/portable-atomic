62c49415fb07e8764ebde53d545064cb1008ba50
Taiki Endo <te316e89@gmail.com>
2022-03-11 04:55:47 +0900

    Update crate-level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,72 @@
-//! Portable atomic types.
+/*!
+Portable atomic types.
+
+- Provide all atomic integer types (`Atomic{I,U}{8,16,32,64}`) for all targets that can use atomic CAS. (i.e., all targets that can use `std`, and most no-std targets)
+- Provide `AtomicI128` and `AtomicU128`.
+- Provide `AtomicF32` and `AtomicF64`. (optional)
+<!-- - Provide generic `Atomic<T>` type. (optional) -->
+- Provide atomic load/store for targets where atomic is not available at all in the standard library. (riscv without A-extension, msp430, avr)
+- Provide atomic CAS for targets where atomic CAS is not available in the standard library. (thumbv6m, riscv without A-extension, msp430, avr) (optional, [single-core only](#optional-cfg))
+
+## 128-bit atomics support (AtomicI128,AtomicU128)
+
+Native 128-bit atomic operations are only available for x86_64 and aarch64 at Rust 1.59+, otherwise the fallback implementation is used.
+
+On x86_64, when the `outline-atomics` optional feature is not enabled and `cmpxchg16b` target feature is not enabled at compile time, this uses the fallback implementation. `cmpxchg16b` is enabled by default only on macOS.
+
+## Optional features
+
+- **`fallback`** *(enabled by default)*<br>
+  Enable fallback implementations.
+
+  Disabling this allows only atomic types for which the platform natively supports atomic operations.
+
+- **`outline-atomics`**<br>
+  Enable dynamic CPU feature detection.
+
+  Note:
+  - Dynamic detection is currently only enabled in nightly, otherwise it works the same as the default.
+  - If the required target features are enabled at compile time, the atomic operations are inlined.
+  - This is compatible with no-std (as with all features except `std` and `parking_lot`).
+
+- **`float`**<br>
+  Provide `AtomicF{32,64}`.
+  Note that most of `fetch_*` operations of atomic floats are implemented using CAS loops, which can be slower than equivalent operations of atomic integers.
+
+<!-- TODO
+- **`generic`**<br>
+  Provides generic `Atomic<T>` type.
+-->
+
+- **`std`**<br>
+  Use `std`.
+
+- **`serde`**<br>
+  Implement `serde::{Serialize,Deserialize}` for atomic types.
+
+  Note:
+  - The MSRV when this feature enables depends on the MSRV of [serde].
+
+- **`parking_lot`**<br>
+  Use [parking_lot] in global locks of fallback implementation.
+
+  Note:
+  - This is not compatible with no-std.
+  - The MSRV when this feature enables depends on the MSRV of [parking_lot].
+
+## Optional cfg
+
+- **`--cfg portable_atomic_unsafe_assume_single_core`**<br>
+  Assume that the target is single-core.
+  When this cfg is enabled, this crate provides atomic CAS for targets where atomic CAS is not available in the standard library.
+
+  Note: This cfg is `unsafe`, and enabling this cfg for multi-core systems is **unsound**.
+
+  This is intentionally not an optional feature. (If this is an optional feature, dependencies can implicitly enable the feature, resulting in the use of unsound code without the end-user being aware of it.)
+
+[parking_lot]: https://github.com/Amanieu/parking_lot
+[serde]: https://github.com/serde-rs/serde
+*/
 
 #![no_std]
 #![doc(test(
