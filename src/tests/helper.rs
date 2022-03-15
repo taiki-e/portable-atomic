a5289b36e073f68d034334c2f4ac6a3ff91fa8ed
Taiki Endo <te316e89@gmail.com>
2022-03-15 23:55:01 +0900

    Tweak assert_panic

--- src/tests/helper.rs
+++ src/tests/helper.rs
@@ -918,11 +918,18 @@ macro_rules! test_atomic_ptr_pub {
     };
 }
 
+#[track_caller]
 pub(crate) fn assert_panic<T: std::fmt::Debug>(f: impl FnOnce() -> T) -> std::string::String {
+    let backtrace = std::env::var_os("RUST_BACKTRACE");
     let hook = std::panic::take_hook();
+    std::env::set_var("RUST_BACKTRACE", "0"); // Suppress backtrace
     std::panic::set_hook(std::boxed::Box::new(|_| {})); // Suppress panic msg
     let res = std::panic::catch_unwind(std::panic::AssertUnwindSafe(f));
     std::panic::set_hook(hook);
+    match backtrace {
+        Some(v) => std::env::set_var("RUST_BACKTRACE", v),
+        None => std::env::remove_var("RUST_BACKTRACE"),
+    }
     let msg = res.unwrap_err();
     msg.downcast_ref::<std::string::String>()
         .cloned()
