cd322d1c9bd246578e3547ff1edd2b6457a8743f
Taiki Endo <te316e89@gmail.com>
2023-02-10 04:25:22 +0900

    codegen: Handle custom targets

--- tools/codegen/src/target_spec.rs
+++ tools/codegen/src/target_spec.rs
@@ -3,18 +3,14 @@ use duct::cmd;
 use serde::Deserialize;
 use strum::{Display, IntoStaticStr};
 
+use crate::file::workspace_root;
+
 pub(crate) fn target_spec_json(target: &str) -> Result<TargetSpec> {
+    let spec_path = workspace_root().join("target-specs").join(target).with_extension("json");
+    let target = if spec_path.exists() { spec_path.as_str() } else { target };
     Ok(serde_json::from_str(
-        &cmd!(
-            "rustc",
-            "--print",
-            "target-spec-json",
-            "-Z",
-            "unstable-options",
-            "--target",
-            &target
-        )
-        .read()?,
+        &cmd!("rustc", "--print", "target-spec-json", "-Z", "unstable-options", "--target", target)
+            .read()?,
     )?)
 }
 
