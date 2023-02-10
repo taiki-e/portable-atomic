a5b7722c6a3292cf800e1b1604e3bd82d5ea25bc
Taiki Endo <te316e89@gmail.com>
2023-02-10 12:26:35 +0900

    aarch64: Support outline atomics on old nightly
    Similar to https://github.com/taiki-e/portable-atomic/commit/3f5d42f70485fd022c2e4a847c81737e3651bbde.

--- src/imp/atomic128/detect/aarch64_std.rs
+++ src/imp/atomic128/detect/aarch64_std.rs
@@ -25,7 +25,10 @@ pub(crate) fn has_lse() -> bool {
     #[allow(unreachable_code)]
     {
         #[cfg(all(
-            not(portable_atomic_no_aarch64_target_feature),
+            not(any(
+                portable_atomic_no_aarch64_target_feature,
+                portable_atomic_unstable_aarch64_target_feature
+            )),
             // https://github.com/rust-lang/stdarch/blob/a0c30f3e3c75adcd6ee7efc94014ebcead61c507/crates/std_detect/src/detect/mod.rs
             // It is fine to use std for targets that we know can be linked to std.
             // Note: std may not be available on tier 3 such as aarch64 FreeBSD/OpenBSD.
