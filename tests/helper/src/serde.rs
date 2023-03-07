fd723d428d0f516b8427f75b218f8ddbe508f328
Taiki Endo <te316e89@gmail.com>
2023-03-07 21:43:03 +0900

    test-helper: Reduce dependencies

--- tests/helper/src/serde.rs
+++ tests/helper/src/serde.rs
@@ -2,12 +2,27 @@ use std::fmt;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(transparent)]
+#[derive(Debug)]
 pub struct DebugPartialEq<T>(pub T);
 
 impl<T: fmt::Debug> PartialEq for DebugPartialEq<T> {
     fn eq(&self, other: &Self) -> bool {
         std::format!("{:?}", self) == std::format!("{:?}", other)
+    }
+}
+impl<T: Serialize> Serialize for DebugPartialEq<T> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.0.serialize(serializer)
+    }
+}
+impl<'de, T: Deserialize<'de>> Deserialize<'de> for DebugPartialEq<T> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        T::deserialize(deserializer).map(Self)
     }
 }
