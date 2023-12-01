1a319c0e097008e8a6227adce3fdbc7b7e458d9e
Taiki Endo <te316e89@gmail.com>
2023-12-01 21:15:05 +0900

    tools: Update cfg list

--- tools/build.sh
+++ tools/build.sh
@@ -235,9 +235,9 @@ if [[ "${rustc_version}" == *"nightly"* ]] || [[ "${rustc_version}" == *"dev"* ]
     # The latest syntax of -Z check-cfg requires 1.75.0-nightly.
     # We only check this on the recent nightly to avoid old clippy bugs.
     # shellcheck disable=SC2207
-    if [[ "${rustc_minor_version}" -ge 75 ]] && [[ -n "${TESTS:-}" ]] && [[ -z "${TARGET_GROUP:-}" ]]; then
+    if [[ "${rustc_minor_version}" -ge 76 ]] && [[ -n "${TESTS:-}" ]] && [[ -z "${TARGET_GROUP:-}" ]]; then
         build_scripts=(build.rs portable-atomic-util/build.rs)
-        check_cfg='-Z unstable-options --check-cfg=cfg(target_pointer_width,values("128")) --check-cfg=cfg(target_arch,values("xtensa")) --check-cfg=cfg(feature,values("cargo-clippy"))'
+        check_cfg='-Z unstable-options --check-cfg=cfg(target_pointer_width,values("128")) --check-cfg=cfg(target_arch,values("xtensa"))'
         known_cfgs+=($(grep -E 'cargo:rustc-cfg=' "${build_scripts[@]}" | sed -E 's/^.*cargo:rustc-cfg=//; s/(=\\)?".*$//' | LC_ALL=C sort -u))
         # TODO: handle multi-line target_feature_if
         known_target_feature_values+=($(grep -E 'target_feature_if\("' "${build_scripts[@]}" | sed -E 's/^.*target_feature_if\(//; s/",.*$/"/' | LC_ALL=C sort -u))
