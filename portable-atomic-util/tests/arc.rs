038a3f1fc02095f4d36aaf5662ea899f6500af25
John Nunley <dev@notgull.net>
2023-12-16 11:02:15 -0800

    Consider alignment when computing into_raw
    This commit fixes #137 by considering alignment when converting the raw
    Arc pointer to and raw its raw representation.
    
    Signed-off-by: John Nunley <dev@notgull.net>

---
+++ portable-atomic-util/tests/arc.rs
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+#![cfg(any(feature = "std", feature = "alloc"))]
+
+use portable_atomic_util::Arc;
+
+#[test]
+fn overaligned() {
+    #[repr(align(128))]
+    struct Aligned(u32);
+
+    let value = Arc::new(Aligned(128));
+    let ptr = Arc::into_raw(value);
+    // SAFETY: `ptr` should always be a valid `Aligned`.
+    assert_eq!(unsafe { (&*ptr).0 }, 128);
+    // SAFETY: `ptr` is a valid reference to an `Arc<Aligned>`.
+    let value = unsafe { Arc::from_raw(ptr) };
+    assert_eq!(value.0, 128);
+}
