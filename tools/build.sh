d58370bc8227b0f0856da0371fff6f4e789cfd46
Taiki Endo <te316e89@gmail.com>
2024-02-22 19:31:22 +0900

    Fix AVR build with LLVM 18
    ```
    error: ran out of registers during register allocation
    ```

--- tools/build.sh
+++ tools/build.sh
@@ -330,7 +330,7 @@ build() {
         if [[ "${llvm_version}" -eq 16 ]]; then
             # https://github.com/rust-lang/compiler-builtins/issues/523
             target_rustflags+=" -C linker-plugin-lto -C codegen-units=1"
-        elif [[ "${llvm_version}" -le 17 ]]; then
+        elif [[ "${llvm_version}" -ge 17 ]]; then
             # https://github.com/rust-lang/rust/issues/88252
             target_rustflags+=" -C opt-level=s"
         fi
