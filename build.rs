3eb8507f91c1ab382d9f455b2311a8664a4c33ff
Taiki Endo <te316e89@gmail.com>
2024-09-28 11:43:29 +0900

    powerpc64: quadword-atomics target_feature now available on rustc side

--- build.rs
+++ build.rs
@@ -47,7 +47,7 @@ fn main() {
 
     if version.minor >= 80 {
         println!(
-            r#"cargo:rustc-check-cfg=cfg(target_feature,values("zaamo","zabha","experimental-zacas","quadword-atomics","fast-serialization","load-store-on-cond","distinct-ops","miscellaneous-extensions-3"))"#
+            r#"cargo:rustc-check-cfg=cfg(target_feature,values("zaamo","zabha","experimental-zacas","fast-serialization","load-store-on-cond","distinct-ops","miscellaneous-extensions-3"))"#
         );
 
         // Custom cfgs set by build script. Not public API.
@@ -343,31 +343,32 @@ fn main() {
             target_feature_fallback("zaamo", false);
         }
         "powerpc64" => {
-            let target_endian =
-                env::var("CARGO_CFG_TARGET_ENDIAN").expect("CARGO_CFG_TARGET_ENDIAN not set");
-            // powerpc64le is pwr8+ by default https://github.com/llvm/llvm-project/blob/llvmorg-19.1.0/llvm/lib/Target/PowerPC/PPC.td#L702
-            // See also https://github.com/rust-lang/rust/issues/59932
-            let mut has_pwr8_features = target_endian == "little";
-            // https://github.com/llvm/llvm-project/commit/549e118e93c666914a1045fde38a2cac33e1e445
-            if let Some(cpu) = &target_cpu() {
-                if let Some(mut cpu_version) = strip_prefix(cpu, "pwr") {
-                    cpu_version = strip_suffix(cpu_version, "x").unwrap_or(cpu_version); // for pwr5x and pwr6x
-                    if let Ok(cpu_version) = cpu_version.parse::<u32>() {
-                        has_pwr8_features = cpu_version >= 8;
+            // target_feature "quadword-atomics" is unstable and available on rustc side since nightly-2024-09-28: https://github.com/rust-lang/rust/pull/130873
+            if !version.probe(83, 2024, 9, 27) || needs_target_feature_fallback(&version, None) {
+                let target_endian =
+                    env::var("CARGO_CFG_TARGET_ENDIAN").expect("CARGO_CFG_TARGET_ENDIAN not set");
+                // powerpc64le is pwr8+ by default https://github.com/llvm/llvm-project/blob/llvmorg-19.1.0/llvm/lib/Target/PowerPC/PPC.td#L702
+                // See also https://github.com/rust-lang/rust/issues/59932
+                let mut has_pwr8_features = target_endian == "little";
+                // https://github.com/llvm/llvm-project/commit/549e118e93c666914a1045fde38a2cac33e1e445
+                if let Some(cpu) = &target_cpu() {
+                    if let Some(mut cpu_version) = strip_prefix(cpu, "pwr") {
+                        cpu_version = strip_suffix(cpu_version, "x").unwrap_or(cpu_version); // for pwr5x and pwr6x
+                        if let Ok(cpu_version) = cpu_version.parse::<u32>() {
+                            has_pwr8_features = cpu_version >= 8;
+                        }
+                    } else {
+                        // https://github.com/llvm/llvm-project/blob/llvmorg-19.1.0/llvm/lib/Target/PowerPC/PPC.td#L702
+                        // https://github.com/llvm/llvm-project/blob/llvmorg-19.1.0/llvm/lib/Target/PowerPC/PPC.td#L483
+                        // On the minimum external LLVM version of the oldest rustc version which we can use asm_experimental_arch
+                        // on this target (see CI config for more), "future" is based on pwr10 features.
+                        // https://github.com/llvm/llvm-project/blob/llvmorg-12.0.0/llvm/lib/Target/PowerPC/PPC.td#L370
+                        has_pwr8_features = cpu == "ppc64le" || cpu == "future";
                     }
-                } else {
-                    // https://github.com/llvm/llvm-project/blob/llvmorg-19.1.0/llvm/lib/Target/PowerPC/PPC.td#L702
-                    // https://github.com/llvm/llvm-project/blob/llvmorg-19.1.0/llvm/lib/Target/PowerPC/PPC.td#L483
-                    // On the minimum external LLVM version of the oldest rustc version which we can use asm_experimental_arch
-                    // on this target (see CI config for more), "future" is based on pwr10 features.
-                    // https://github.com/llvm/llvm-project/blob/llvmorg-12.0.0/llvm/lib/Target/PowerPC/PPC.td#L370
-                    has_pwr8_features = cpu == "ppc64le" || cpu == "future";
                 }
+                // lqarx and stqcx.
+                target_feature_fallback("quadword-atomics", has_pwr8_features);
             }
-            // As of rustc 1.80, target_feature "quadword-atomics" is not available on rustc side:
-            // https://github.com/rust-lang/rust/blob/1.80.0/compiler/rustc_target/src/target_features.rs#L253
-            // lqarx and stqcx.
-            target_feature_fallback("quadword-atomics", has_pwr8_features);
         }
         "s390x" => {
             // https://github.com/llvm/llvm-project/blob/llvmorg-19.1.0/llvm/lib/Target/SystemZ/SystemZFeatures.td
