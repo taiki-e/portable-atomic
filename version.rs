9f80e1e0db6ca3e326f5c19f221519c67a59dc7a
Taiki Endo <te316e89@gmail.com>
2025-06-05 23:54:20 +0900

    Update build script for Rust 1.87

--- version.rs
+++ version.rs
@@ -35,8 +35,8 @@ impl Version {
     // the rustc version, we assume this is the current version.
     // It is no problem if this is older than the actual latest stable.
     // LLVM version is assumed to be the minimum external LLVM version:
-    // https://github.com/rust-lang/rust/blob/1.86.0/src/bootstrap/src/core/build_steps/llvm.rs#L646
-    pub(crate) const LATEST: Self = Self::stable(86, 18);
+    // https://github.com/rust-lang/rust/blob/1.87.0/src/bootstrap/src/core/build_steps/llvm.rs#L617
+    pub(crate) const LATEST: Self = Self::stable(87, 18);
 
     pub(crate) const fn stable(rustc_minor: u32, llvm_major: u32) -> Self {
         Self { minor: rustc_minor, nightly: false, commit_date: Date::UNKNOWN, llvm: llvm_major }
