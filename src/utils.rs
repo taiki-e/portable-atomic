d118cf01f852ef4cc1fa4e0a08f80f5e2a5c8f41
Taiki Endo <te316e89@gmail.com>
2025-09-07 17:08:30 +0900

    Add static assertion for zero_extend64_ptr

--- src/utils.rs
+++ src/utils.rs
@@ -333,6 +333,12 @@ pub(crate) fn upgrade_success_ordering(success: Ordering, failure: Ordering) -> 
     }
 }
 
+#[cfg(not(portable_atomic_no_asm_maybe_uninit))]
+#[cfg(target_pointer_width = "32")]
+// SAFETY: MaybeUninit returned by zero_extend64_ptr is always initialized.
+const _: () = assert!(unsafe {
+    zero_extend64_ptr(ptr::without_provenance_mut(!0)).assume_init() == !0_u32 as u64
+});
 /// Zero-extends the given 32-bit pointer to `MaybeUninit<u64>`.
 /// This is used for 64-bit architecture's 32-bit ABI (e.g., AArch64 ILP32 ABI).
 /// See ptr_reg! macro in src/gen/utils.rs for details.
@@ -340,7 +346,7 @@ pub(crate) fn upgrade_success_ordering(success: Ordering, failure: Ordering) -> 
 #[cfg(target_pointer_width = "32")]
 #[allow(dead_code)]
 #[inline]
-pub(crate) fn zero_extend64_ptr(v: *mut ()) -> core::mem::MaybeUninit<u64> {
+pub(crate) const fn zero_extend64_ptr(v: *mut ()) -> core::mem::MaybeUninit<u64> {
     #[repr(C)]
     struct ZeroExtended {
         #[cfg(target_endian = "big")]
@@ -474,8 +480,29 @@ pub(crate) mod ptr {
     use core::mem;
     #[cfg(not(portable_atomic_no_strict_provenance))]
     #[allow(unused_imports)]
-    pub(crate) use core::ptr::{with_exposed_provenance, with_exposed_provenance_mut};
+    pub(crate) use core::ptr::{
+        with_exposed_provenance, with_exposed_provenance_mut, without_provenance_mut,
+    };
 
+    #[cfg(portable_atomic_no_strict_provenance)]
+    #[inline(always)]
+    #[must_use]
+    pub(crate) const fn without_provenance_mut<T>(addr: usize) -> *mut T {
+        // An int-to-pointer transmute currently has exactly the intended semantics: it creates a
+        // pointer without provenance. Note that this is *not* a stable guarantee about transmute
+        // semantics, it relies on sysroot crates having special status.
+        // SAFETY: every valid integer is also a valid pointer (as long as you don't dereference that
+        // pointer).
+        #[cfg(miri)]
+        unsafe {
+            mem::transmute(addr)
+        }
+        // const transmute requires Rust 1.56.
+        #[cfg(not(miri))]
+        {
+            addr as *mut T
+        }
+    }
     #[cfg(portable_atomic_no_strict_provenance)]
     #[inline(always)]
     #[must_use]
