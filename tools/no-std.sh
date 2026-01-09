3a93608ecc326e98acf5106b4ca08891a277125c
Taiki Endo <te316e89@gmail.com>
2026-01-09 22:09:31 +0900

    ci: Use ghcr.io/taiki-e/qemu-system to install qemu-system

--- tools/no-std.sh
+++ tools/no-std.sh
@@ -385,24 +385,8 @@ EOF
             fi
             ;;
         esac
-        local arch
-        case "${target}" in
-          riscv32*) arch=riscv32 ;;
-          riscv64*) arch=riscv64 ;;
-          *) bail "${target}" ;;
-        esac
-        # Support for Zabha extension requires LLVM 19+ and QEMU 9.1+.
-        # https://github.com/qemu/qemu/commit/be4a8db7f304347395b081ae5848bad2f507d0c4
-        # Old QEMU's Zacas support is also buggy.
-        qemu_version=$(qemu-system-"${arch}" --version | sed -En '1 s/QEMU emulator version [^ ]+ \(v([^ )]+)\)/\1/p')
-        if [[ -z "${qemu_version}" ]]; then
-          qemu_version=$(qemu-system-"${arch}" --version | sed -En '1 s/QEMU emulator version ([^ )]+)/\1/p')
-        fi
-        if [[ ! "${qemu_version}" =~ ^(9\.[^0]|[1-9][0-9]+\.) ]]; then
-          subcmd=build
-        fi
+        # Support for Zabha extension requires LLVM 19+.
         if [[ "${llvm_version}" -ge 19 ]]; then
-          export "CARGO_TARGET_${target_upper}_RUNNER"="qemu-system-${arch} -M virt -cpu max -display none -semihosting -kernel"
           CARGO_TARGET_DIR="${target_dir}/no-std-test-zabha" \
             RUSTFLAGS="${target_rustflags} -C target-feature=+zaamo,+zabha" \
             x_cargo "${args[@]}" "$@"
