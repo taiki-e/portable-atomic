6d1418773013359fb576a5d3b955568fe344171f
Taiki Endo <te316e89@gmail.com>
2026-07-16 00:36:09 +0900

    fallback: Move shared code to seq_lock_common.rs and improve tests

--- src/imp/fallback/seq_lock.rs
+++ src/imp/fallback/seq_lock.rs
@@ -2,18 +2,11 @@
 
 // Adapted from https://github.com/crossbeam-rs/crossbeam/blob/crossbeam-utils-0.8.21/crossbeam-utils/src/atomic/seq_lock.rs.
 
-use core::{
-    mem::ManuallyDrop,
-    sync::atomic::{AtomicU64, Ordering},
-};
+include!("seq_lock_common.rs");
 
-use super::utils::Backoff;
-#[cfg(portable_atomic_unsafe_assume_privileged)]
-use crate::imp::interrupt::arch as interrupt;
+use core::sync::atomic::AtomicU64;
 
 pub(super) type State = u64;
-
-const LOCKED: State = 1;
 
 /// A simple stamped lock.
 pub(super) struct SeqLock {
@@ -53,116 +46,11 @@ impl SeqLock {
         crate::fence(Ordering::Acquire);
         self.state.load(Ordering::Relaxed) == stamp
     }
-
-    /// Grabs the lock for writing.
-    #[inline]
-    pub(super) fn write(&self) -> SeqLockWriteGuard<'_> {
-        // Get current interrupt state and disable interrupts when the user
-        // explicitly declares that privileged instructions are available.
-        #[cfg(portable_atomic_unsafe_assume_privileged)]
-        let interrupt_state = interrupt::disable();
-
-        let mut backoff = Backoff::new();
-        loop {
-            let previous = self.state.swap(LOCKED, Ordering::Acquire);
-
-            if previous != LOCKED {
-                crate::fence(Ordering::Release);
-
-                return SeqLockWriteGuard {
-                    lock: self,
-                    state: previous,
-                    #[cfg(portable_atomic_unsafe_assume_privileged)]
-                    interrupt_state,
-                };
-            }
-
-            while self.state.load(Ordering::Relaxed) == LOCKED {
-                backoff.snooze();
-            }
-        }
-    }
-}
-
-/// An RAII guard that releases the lock and increments the stamp when dropped.
-#[must_use]
-pub(super) struct SeqLockWriteGuard<'a> {
-    /// The parent lock.
-    lock: &'a SeqLock,
-
-    /// The stamp before locking.
-    state: State,
-
-    /// The interrupt state before disabling.
-    #[cfg(portable_atomic_unsafe_assume_privileged)]
-    interrupt_state: interrupt::State,
 }
 
 impl SeqLockWriteGuard<'_> {
-    /// Releases the lock without incrementing the stamp.
     #[inline]
-    pub(super) fn abort(self) {
-        // We specifically don't want to call drop(), since that's
-        // what increments the stamp.
-        let this = ManuallyDrop::new(self);
-
-        // Restore the stamp.
-        //
-        // Release ordering for synchronizing with `optimistic_read`.
-        this.lock.state.store(this.state, Ordering::Release);
-
-        // Restore interrupt state.
-        // SAFETY: the state was retrieved by the previous `disable`.
-        #[cfg(portable_atomic_unsafe_assume_privileged)]
-        unsafe {
-            interrupt::restore(this.interrupt_state);
-        }
-    }
-}
-
-impl Drop for SeqLockWriteGuard<'_> {
-    #[inline]
-    fn drop(&mut self) {
-        // Release the lock and increment the stamp.
-        //
-        // Release ordering for synchronizing with `optimistic_read`.
-        self.lock.state.store(self.state.wrapping_add(2), Ordering::Release);
-
-        // Restore interrupt state.
-        // SAFETY: the state was retrieved by the previous `disable`.
-        #[cfg(portable_atomic_unsafe_assume_privileged)]
-        unsafe {
-            interrupt::restore(self.interrupt_state);
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::SeqLock;
-
-    #[test]
-    fn smoke() {
-        let lock = SeqLock::new();
-        let before = lock.optimistic_read().unwrap();
-        assert!(lock.validate_read(before));
-        {
-            let _guard = lock.write();
-        }
-        assert!(!lock.validate_read(before));
-        let after = lock.optimistic_read().unwrap();
-        assert_ne!(before, after);
-    }
-
-    #[test]
-    fn test_abort() {
-        let lock = SeqLock::new();
-        let before = lock.optimistic_read().unwrap();
-        {
-            let guard = lock.write();
-            guard.abort();
-        }
-        let after = lock.optimistic_read().unwrap();
-        assert_eq!(before, after, "aborted write does not update the stamp");
+    fn next_stamp(&self) -> State {
+        self.state.wrapping_add(2)
     }
 }
--- src/imp/fallback/seq_lock_wide.rs
+++ src/imp/fallback/seq_lock_wide.rs
@@ -2,18 +2,11 @@
 
 // Adapted from https://github.com/crossbeam-rs/crossbeam/blob/crossbeam-utils-0.8.21/crossbeam-utils/src/atomic/seq_lock_wide.rs.
 
-use core::{
-    mem::ManuallyDrop,
-    sync::atomic::{AtomicU32, Ordering},
-};
+include!("seq_lock_common.rs");
 
-use super::utils::Backoff;
-#[cfg(portable_atomic_unsafe_assume_privileged)]
-use crate::imp::interrupt::arch as interrupt;
+use core::sync::atomic::AtomicU32;
 
 pub(super) type State = u32;
-
-const LOCKED: State = 1;
 
 /// A simple stamped lock.
 ///
@@ -27,13 +20,13 @@ pub(super) struct SeqLock {
     ///
     /// All bits except the least significant one hold the current stamp. When locked, the state_lo
     /// equals 1 and doesn't contain a valid stamp.
-    state_lo: AtomicU32,
+    state: AtomicU32,
 }
 
 impl SeqLock {
     #[inline]
     pub(super) const fn new() -> Self {
-        Self { state_hi: AtomicU32::new(0), state_lo: AtomicU32::new(0) }
+        Self { state_hi: AtomicU32::new(0), state: AtomicU32::new(0) }
     }
 
     /// If not locked, returns the current stamp.
@@ -48,7 +41,7 @@ impl SeqLock {
         // happens before now; and therefore, (2) if `state_lo` is even, all writes within the
         // critical section of (`state_hi`, `state_lo`) happens before now.
         let state_hi = self.state_hi.load(Ordering::Acquire);
-        let state_lo = self.state_lo.load(Ordering::Acquire);
+        let state_lo = self.state.load(Ordering::Acquire);
         if state_lo == LOCKED { None } else { Some((state_hi, state_lo)) }
     }
 
@@ -68,7 +61,7 @@ impl SeqLock {
         // around.
         //
         // If (2) is the case, the acquire ordering ensures we see the new value of `state_hi`.
-        let state_lo = self.state_lo.load(Ordering::Acquire);
+        let state_lo = self.state.load(Ordering::Acquire);
 
         // If (2) is the case and `state_hi` coincides with `stamp.0`, then `state_hi` also wrapped
         // around, which we give up to correctly validate the read.
@@ -79,79 +72,12 @@ impl SeqLock {
         // section of `(stamp.0, stamp.1)`.
         (state_hi, state_lo) == stamp
     }
-
-    /// Grabs the lock for writing.
-    #[inline]
-    pub(super) fn write(&self) -> SeqLockWriteGuard<'_> {
-        // Get current interrupt state and disable interrupts when the user
-        // explicitly declares that privileged instructions are available.
-        #[cfg(portable_atomic_unsafe_assume_privileged)]
-        let interrupt_state = interrupt::disable();
-
-        let mut backoff = Backoff::new();
-        loop {
-            let previous = self.state_lo.swap(LOCKED, Ordering::Acquire);
-
-            if previous != LOCKED {
-                // To synchronize with the acquire fence in `validate_read` via any modification to
-                // the data at the critical section of `(state_hi, previous)`.
-                crate::fence(Ordering::Release);
-
-                return SeqLockWriteGuard {
-                    lock: self,
-                    state_lo: previous,
-                    #[cfg(portable_atomic_unsafe_assume_privileged)]
-                    interrupt_state,
-                };
-            }
-
-            while self.state_lo.load(Ordering::Relaxed) == LOCKED {
-                backoff.snooze();
-            }
-        }
-    }
-}
-
-/// An RAII guard that releases the lock and increments the stamp when dropped.
-#[must_use]
-pub(super) struct SeqLockWriteGuard<'a> {
-    /// The parent lock.
-    lock: &'a SeqLock,
-
-    /// The stamp before locking.
-    state_lo: State,
-
-    /// The interrupt state before disabling.
-    #[cfg(portable_atomic_unsafe_assume_privileged)]
-    interrupt_state: interrupt::State,
 }
 
 impl SeqLockWriteGuard<'_> {
-    /// Releases the lock without incrementing the stamp.
     #[inline]
-    pub(super) fn abort(self) {
-        // We specifically don't want to call drop(), since that's
-        // what increments the stamp.
-        let this = ManuallyDrop::new(self);
-
-        // Restore the stamp.
-        //
-        // Release ordering for synchronizing with `optimistic_read`.
-        this.lock.state_lo.store(this.state_lo, Ordering::Release);
-
-        // Restore interrupt state.
-        // SAFETY: the state was retrieved by the previous `disable`.
-        #[cfg(portable_atomic_unsafe_assume_privileged)]
-        unsafe {
-            interrupt::restore(this.interrupt_state);
-        }
-    }
-}
-
-impl Drop for SeqLockWriteGuard<'_> {
-    #[inline]
-    fn drop(&mut self) {
-        let state_lo = self.state_lo.wrapping_add(2);
+    fn next_stamp(&self) -> State {
+        let state_lo = self.state.wrapping_add(2);
 
         // Increase the high bits if the low bits wrap around.
         //
@@ -161,46 +87,6 @@ impl Drop for SeqLockWriteGuard<'_> {
             self.lock.state_hi.store(state_hi.wrapping_add(1), Ordering::Release);
         }
 
-        // Release the lock and increment the stamp.
-        //
-        // Release ordering for synchronizing with `optimistic_read`.
-        self.lock.state_lo.store(state_lo, Ordering::Release);
-
-        // Restore interrupt state.
-        // SAFETY: the state was retrieved by the previous `disable`.
-        #[cfg(portable_atomic_unsafe_assume_privileged)]
-        unsafe {
-            interrupt::restore(self.interrupt_state);
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::SeqLock;
-
-    #[test]
-    fn smoke() {
-        let lock = SeqLock::new();
-        let before = lock.optimistic_read().unwrap();
-        assert!(lock.validate_read(before));
-        {
-            let _guard = lock.write();
-        }
-        assert!(!lock.validate_read(before));
-        let after = lock.optimistic_read().unwrap();
-        assert_ne!(before, after);
-    }
-
-    #[test]
-    fn test_abort() {
-        let lock = SeqLock::new();
-        let before = lock.optimistic_read().unwrap();
-        {
-            let guard = lock.write();
-            guard.abort();
-        }
-        let after = lock.optimistic_read().unwrap();
-        assert_eq!(before, after, "aborted write does not update the stamp");
+        state_lo
     }
 }
