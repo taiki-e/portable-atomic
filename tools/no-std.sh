c6fab541d4c36704dcfe086028b0575df76947b9
Taiki Endo <te316e89@gmail.com>
2026-08-09 14:36:44 +0900

    tests: AArch64 L4Re is now available as builtin target

--- tools/no-std.sh
+++ tools/no-std.sh
@@ -59,7 +59,7 @@ default_targets=(
   msp430-none-elf
 
   # l4re
-  # aarch64-unknown-l4re-uclibc # custom target # TODO: version `GLIBC_2.42' not found
+  # aarch64-unknown-l4re-uclibc # TODO: version `GLIBC_2.42' not found
 )
 
 x() {
